49e3f83b2d00a34ea0ab8440bc3f1673fa4f180c
Lucas Weber <Lucaswf659@gmail.com>
2020-08-04 10:59:25 -0300

    Angular 9

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './navigation/navigation.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
