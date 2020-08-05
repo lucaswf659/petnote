102b5bad067c866f7edca4f7dbe026d4374db21b
Lucas Weber <Lucaswf659@gmail.com>
2020-08-05 16:33:51 -0300

    chore: add jest

---
+++ backend/jest.config.js
@@ -0,0 +1,12 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  // coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
